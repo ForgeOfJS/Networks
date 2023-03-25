be065b64a5afbe07afd03396fd6c6afd83458759
Josue Soto <joshsoto6738@gmail.com>
2023-03-25 00:46:38 -0600

    Initial add

---
+++ Client/rft1Client.py
@@ -0,0 +1,37 @@
+import socket, os, sys
+
+print("Provide Server IP: ", end="")
+HOST = input()
+print("Provide Port#: ", end="")
+PORT = int(input())
+
+#Look for and establish connection to server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+print("You are now connected! Enter your commands now.")
+#Prompt user
+print("RFTCli>", end = " ")
+clientRequest = input()
+#Turn request into bytes and send request to server
+message = bytes(clientRequest, 'utf-8')
+clientRequestSeg = clientRequest.split(" ")
+client.sendall(message)
+#If client requests a CLOSE then client should exit
+if clientRequest == "CLOSE": sys.exit()
+#Begin the file transfer, i.e. create the file to have data placed into.
+returnFilePath = f'{os.getcwd()}\FileStorage\copy_{clientRequestSeg[1]}'
+file = open(returnFilePath, 'wb')
+#Start transfering packets
+while True:
+    #Get packets
+    data = client.recv(1024)
+    #Edge case, if command failed or file is divisible by 1000 bytes then stop file transfer
+    if data == b'!': break
+    print(len(data))
+    file.write(data)
+    #Once a non 1000 byte packet is recieved then stop the transfer. 
+    if len(data) != 1000:
+        break
+#File transfer is completed.
+file.close()
+print(f'Recieved {clientRequestSeg[1]}')
---
+++ Server/rft1Server.py
@@ -0,0 +1,69 @@
+import socket, sys, os, time
+
+HOST = "127.0.0.1"
+print("Listen at Port#: ", end="")
+PORT = int(input())
+
+#Start server and listen for a connection.
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+    server.bind((HOST, PORT))
+    server.listen()
+    print(f'Listening for connection at {PORT}...')
+    while True:
+        #Connection is established.
+        conn, addr = server.accept()
+        print(f"Connection accepted from {addr}.")
+        data = conn.recv(1024)
+        message = data.decode()
+        #Check for CLOSE command and close connection if so.
+        if message == 'CLOSE':
+            print("Connection closed, See you later!")
+            conn.close()
+            sys.exit()
+        #Check for RETR command and perform opearation if so, otherwise send nothing.
+        if message[:4] == "RETR":
+            message = message.split(" ")
+            print(f"Asking for file {message[1]}")
+            #Get file name and check in the predetermined file storage if file exists
+            requestedFilePath = f'{os.getcwd()}\FileStorage\{message[1]}'
+            #If file exists begin the transfer, otherwise send nothing.
+            if os.path.isfile(requestedFilePath):
+                with open(requestedFilePath, 'rb') as file:
+                    print("Sending the file...")
+                    #Open and read the file as bytes
+                    fileContents = file.read()
+                    fileBytes = bytes(fileContents)
+                    fileSize = len(fileBytes)
+                    packets = []
+                    #Check if filesize is smaller than largest packet byte size allowed
+                    #If so, then add to packets and send to client.
+                    if fileSize <= 1000:
+                        packets.append(fileBytes)
+                    else:
+                        #Otherwise split file into 1000 bytes
+                        #Find # of packets needed
+                        numPackets = fileSize // 1000
+                        #Segment file into 1000 bytes sized packets
+                        for i in range(0, numPackets):
+                            print()
+                            start = i * 1000
+                            end = start + 1000
+                            packets.append(bytes(fileBytes[start:end]))
+                        #Add last packet that covers the < 1000 bytes left over.
+                        if fileSize % 1000 != 0: packets.append(fileBytes[end:len(fileBytes)])
+                    #Send the packets back to back
+                    for packet in packets:
+                        print(len(packet))
+                        conn.send(packet)
+                    #Edge case, if packet is divisble by 1000 bytes, send "!" as EOF
+                    if fileSize % 1000 == 0:
+                        time.sleep(1)
+                        conn.send(b'!')
+                    print("Transfer Complete!")
+            else:
+                print("File not found.")
+                conn.send(b'!')
+
+        else:
+            print("Command not recongnized.")
+            conn.send(b'!')
