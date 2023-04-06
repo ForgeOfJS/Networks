cf437ebc7dd728061e4c392dbeb7ec347ead5827
joelaaguirre12 <joelaaguirre12@gmail.com>
2023-04-05 18:50:23 -0600

    Made more modifications

--- Client/rft1Client.py
+++ Client/rft1Client.py
@@ -4,10 +4,8 @@ from timer import *
 from udt import *
 
 
-print("Provide Server IP: ", end="")
-HOST = input()
-print("Provide Port#: ", end="")
-PORT = int(input())
+HOST = input("Provide Server IP: ")
+PORT = int(input("Provide Port#: "))
 address = (HOST, PORT)
 
 # Look for and establish connection to server
@@ -15,11 +13,11 @@ client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 print("You are now connected! Enter your commands now.")
 
 # Prompt user
-print("RFTCli>", end=" ")
-clientRequest = input().split(" ")
+clientRequest = input("RFTCli>")
+clientRequestSeq = clientRequest.split(" ")
 
 # Turn request into bytes and send request to server
-message = make(1, clientRequest)
+message = make(1, clientRequest.encode())
 send(message, client, address)
 
 # If client requests a CLOSE then client should exit
@@ -27,8 +25,9 @@ if clientRequest == "CLOSE":
     sys.exit()
 
 # Begin the file transfer, i.e. create the file to have data placed into.
-returnFilePath = f'{os.getcwd()}\FileStorage\copy_{clientRequest[1]}'
+returnFilePath = f'{os.getcwd()}\FileStorage\copy_{clientRequestSeq[1]}'
 file = open(returnFilePath, 'wb')
+
 # Start transferring packets
 while True:
     # Get packets
@@ -47,4 +46,4 @@ while True:
         break
 # File transfer is completed.
 file.close()
-print(f'Received {clientRequest[1]}')
+print(f'Received {clientRequestSeq[1]}')
--- Server/rft1Server.py
+++ Server/rft1Server.py
@@ -1,48 +1,17 @@
 import socket, os, sys, random
-DROP_PROB = 2
-
-
-# Creates a packet from a sequence number and byte data
-def make(seq_num, data=b''):
-    seq_bytes = seq_num.to_bytes(4, byteorder = 'little', signed = True)
-    return seq_bytes + data
-
-
-# Creates an empty packet
-def make_empty():
-    return b''
-
-
-# Extracts sequence number and data from a non-empty packet
-def extract(packet):
-    seq_num = int.from_bytes(packet[0:4], byteorder = 'little', signed = True)
-    return seq_num, packet[4:]
-
-
-# Send a packet across the unreliable channel
-# Packet may be lost
-def send(packet, sock, addr):
-    if random.randint(0, 10) > DROP_PROB:
-        sock.sendto(packet, addr)
-    return
-
-
-# Receive a packet from the unreliable channel
-def recv(sock):
-    packet, addr = sock.recvfrom(1024)
-    return packet, addr
+from packet import *
+from timer import *
+from udt import *
 
 
 HOST = "127.0.0.1"
-print("Listen at Port#: ", end="")
-PORT = int(input())
+PORT = int(input("Listen at Port#: "))
+address = (HOST, PORT)
 
 #Start server and listen for a connection.
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server:
-    server.bind((HOST, PORT))
-
     while True:
-        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+        data, addr = server.recvfrom(1024) # buffer size is 1024 bytes
 
     print(f'Listening for connection at {PORT}...')
     while True:
